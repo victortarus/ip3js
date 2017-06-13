ed39af418a721ce9a3ccdbd4658845aeaf6ca3f7
victor tarus <victorkibet25@gmail.com>
2017-06-13 09:54:24 +0300

    created an answer tile to delete an answer

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isAnswerDialogShowing:false,
+  action:{
+    showDialogBox:function(){
+      this.set("isAnswerDialogShowing",true);
+    }
+  }
 });
