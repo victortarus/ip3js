e5cacfa9312334f9f2504a8babf7b91c28ca5ca2
victor tarus <victorkibet25@gmail.com>
2017-06-13 10:41:47 +0300

    finished adding fn

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer:false,
+  action:{
+  answerFormShow(){
+    this.set('addNewAnswer',true);
+  }
+ },
+ saveAnswer1(){
+   var params ={
+     author.this.get('author'),
+     answer.this.get('answer'),
+     content.this.get('content'),
+   };
+   this.set('addNewAnswer',false);
+   this.sendAction('saveAnswer2',params);
+ }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,11 @@ export default Ember.Route.extend({
       });
     },
     actions:{
+      saveAnswer3(params){
+        var newAnswer=this.store.createRecord('answer',params);
+        newAnswer.save();
+        this.transitionTo('index');
+      },
       destroyAnswer(answer){
         answer.destroyRecord();
         this.transitionTo('index');
