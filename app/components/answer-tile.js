e7f47d0f77d8106cd044ce70e56432cbd5c3bdc8
victor tarus <victorkibet25@gmail.com>
2017-06-13 10:00:34 +0300

    added a delete trigger

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -6,8 +6,13 @@ export default Ember.Component.extend({
     showDialogBox:function(){
       this.set("isAnswerDialogShowing",true);
     }
-  }
+  },
   answerDialogBox:function(){
     this.set("isAnswerDialogShowing",false);
-  }
+  },
+  delete(answer) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    }
 });
